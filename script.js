281d7d8053555083c8b586f1372dfc72160263f4
Thiago Mesquita <91266058+thiagojbmesquita@users.noreply.github.com>
2021-09-26 11:08:17 -0300

    Adicionei os arquivos do Certificard

---
+++ script.js
@@ -0,0 +1,11 @@
+// <li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/RwgZEmO"> 🔢 Calculadora de Média </a></li>
+//<li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/RwgZEmO"> 🔢 Calculadora de Média </a></li>
+//   <li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/KKqyXKd">🧠 Mentalista </a></li>
+//  <li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/powrXeQ"> 💰 Conversor de moeda </a></li> <li class="projetos-itens">  <a href="https://codepen.io/thiagojbmesquita/pen/RwgxBjN">🎥 Aluraflix v1</a></li>;
+//  <li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/YzQLqYx">🎥 Aluraflix v2</a></li>
+//  <li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/gORzmNW"> 💯 Tabela de classificação</a></li>
+// <li class="projetos-itens"><a href="https://codepen.io/thiagojbmesquita/pen/NWgBVVY"> 🎴 Super Trunfo</a></li>
+
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
